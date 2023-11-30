b77c96ee698c3671017e80dcec011d271ac8cff0
nero <sergeinerobeev@gmail.com>
2023-11-30 15:10:12 +0100

    add Idea correct

--- src/org/javaacademy/wonder_field/Game.java
+++ src/org/javaacademy/wonder_field/Game.java
@@ -7,37 +7,35 @@ import java.util.Scanner;
  */
 
 public class Game {
-	private final int player1 = 1;
-	private final int player2 = 2;
-	private final int player3 = 3;
-	private final int round1 = 1;
-	private final int round2 = 2;
-	private final int round3 = 3;
-	private final int round4 = 4;
-	private final int groupRound1 = 1;
-	private final int groupRound2 = 2;
-	private final int groupRound3 = 3;
-	private final int indexOfFinalRound1 = 1;
-	private final int indexOfFinalRound2 = 2;
-	private final int indexOfFinalRound3 = 3;
+		private final int player1 = 1;
+		private final int player2 = 2;
+		private final int player3 = 3;
+		private final int round1 = 1;
+		private final int round2 = 2;
+		private final int round3 = 3;
+		private final int round4 = 4;
+		private final int groupRound1 = 1;
+		private final int groupRound2 = 2;
+		private final int groupRound3 = 3;
+		private final int indexOfFinalRound1 = 1;
+		private final int indexOfFinalRound2 = 2;
+		private final int indexOfFinalRound3 = 3;
 
-	static Scanner scanner = new Scanner(System.in);
-	private final DataGame dataGame;
+		private final static Scanner scanner = new Scanner(System.in);
+		private final DataGame dataGame;
 
-	public Game() {
-		this.dataGame = new DataGame();
-	}
+		public Game() {
+				this.dataGame = new DataGame();
+		}
 
-	// ИНИЦИАЛИЗАЦИЯ ИГРЫ
-	public void init() {
+		// ИНИЦИАЛИЗАЦИЯ ИГРЫ
+		public void init() {
 
-		dataGame.setDataGame();
-		String[] questions = dataGame.getQuestions();
-		String[] answers = dataGame.getAnswers();
-		System.out.println(Arrays.toString(questions));
-		System.out.println(Arrays.toString(answers));
+				dataGame.setDataGame();
+				String[] questions = dataGame.getQuestions();
+				String[] answers = dataGame.getAnswers();
 
-	}
+		}
 
 
 }
--- src/org/javaacademy/wonder_field/Runner.java
+++ src/org/javaacademy/wonder_field/Runner.java
@@ -4,12 +4,12 @@ package org.javaacademy.wonder_field;
  * TОЧКА ВХОДА-ЗАПУСК ПРОГРАММЫ
  */
 public class Runner {
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 
 
-		Game game = new Game();
-		game.init();
+				Game game = new Game();
+				game.init();
 
 
-	}
+		}
 }
